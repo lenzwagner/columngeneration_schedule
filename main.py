969fb0f129ebd2eac641a540d85b207a6a2ebabc
lenzwagner <lorenz.wagner99@gmail.com>
2024-03-06 16:01:00 +0100

    Change "addConstr" to "addLConstr" for better performance.

--- main.py
+++ main.py
@@ -64,7 +64,7 @@ class MasterProblem:
 
     def generateConstraints(self):
         for i in self.nurses:
-            self.cons_lmbda[i] = self.model.addConstr(1 == gu.quicksum(self.lmbda[i, r] for r in self.rosterinitial), name = "lmb("+str(i)+")")
+            self.cons_lmbda[i] = self.model.addLConstr(1 == gu.quicksum(self.lmbda[i, r] for r in self.rosterinitial), name = "lmb("+str(i)+")")
         for t in self.days:
             for s in self.shifts:
                 self.cons_demand[t, s] = self.model.addConstr(
@@ -148,8 +148,6 @@ class MasterProblem:
     def addLambda(self, index, itr):
         self.nurseIndex = index
         self.rosterIndex = itr + 1
-        print(f"I: {self.nurseIndex}")
-        print(f"R: {self.rosterIndex}")
         self.newlmbcoef = 1.0
         current_lmb_cons = self.cons_lmbda[self.nurseIndex]
         expr = self.model.getRow(current_lmb_cons)
@@ -215,26 +213,27 @@ class Subproblem:
         self.model.update()
 
     def generateVariables(self):
-        self.x = self.model.addVars([self.index], self.days, self.shifts, vtype=GRB.BINARY, name='x')
-        self.y = self.model.addVars([self.index], self.days, vtype=GRB.BINARY, name='y')
-        self.mood = self.model.addVars([self.index], self.days, vtype=GRB.CONTINUOUS, lb=0, name='mood')
-        self.motivation = self.model.addVars([self.index], self.days, self.shifts, [self.itr], vtype=GRB.CONTINUOUS, lb=0, name='motivation')
+        self.x = self.model.addVars([self.index], self.days, self.shifts, vtype=gu.GRB.BINARY, name='x')
+        self.y = self.model.addVars([self.index], self.days, vtype=gu.GRB.BINARY, name='y')
+        self.mood = self.model.addVars([self.index], self.days, vtype=gu.GRB.CONTINUOUS, lb=0, name='mood')
+        self.motivation = self.model.addVars([self.index], self.days, self.shifts, [self.itr], vtype=gu.GRB.CONTINUOUS, lb=0, name='motivation')
 
     def generateConstraints(self):
         for i in [self.index]:
             for t in self.days:
-                self.model.addConstr(self.mood[i, t] == 1- self.alpha[i, t])
-                self.model.addConstr(quicksum(self.x[i, t, s] for s in self.shifts) == self.y[i, t])
-                self.model.addConstr(gu.quicksum(self.x[i, t, s] for s in self.shifts) <= 1)
-
-            for t in range(1, len(self.days) - self.Max + 1):
-                self.model.addConstr(gu.quicksum(self.y[i, u] for u in range(t, t + 1 + self.Max)) <= self.Max)
-            self.model.addLConstr(quicksum(self.y[i, t] for t in self.days) >= self.Min)
-            for t in self.days:
+                self.model.addLConstr(self.mood[i, t] == 1- self.alpha[i, t])
+                self.model.addLConstr(quicksum(self.x[i, t, s] for s in self.shifts) == self.y[i, t])
+                self.model.addLConstr(gu.quicksum(self.x[i, t, s] for s in self.shifts) <= 1)
                 for s in self.shifts:
-                    self.model.addLConstr(self.motivation[i, t, s, self.itr] >= self.mood[i, t] - self.M * (1 - self.x[i, t, s]))
-                    self.model.addLConstr(self.motivation[i, t, s, self.itr] <= self.mood[i, t] + self.M * (1 - self.x[i, t, s]))
+                    self.model.addLConstr(
+                        self.motivation[i, t, s, self.itr] >= self.mood[i, t] - self.M * (1 - self.x[i, t, s]))
+                    self.model.addLConstr(
+                        self.motivation[i, t, s, self.itr] <= self.mood[i, t] + self.M * (1 - self.x[i, t, s]))
                     self.model.addLConstr(self.motivation[i, t, s, self.itr] <= self.x[i, t, s])
+            for t in range(1, len(self.days) - self.Max + 1):
+                self.model.addLConstr(gu.quicksum(self.y[i, u] for u in range(t, t + 1 + self.Max)) <= self.Max)
+            self.model.addLConstr(self.Min <= quicksum(self.y[i, t] for t in self.days))
+
 
     def generateObjective(self):
         self.model.setObjective(
@@ -261,7 +260,7 @@ class Subproblem:
         self.model.setParam('MIPGap', EPS)
         self.model.Params.OutputFlag = 0
         self.model.optimize()
-        if self.model.status == GRB.OPTIMAL:
+        if self.model.status == gu.GRB.OPTIMAL:
             print("Optimal solution found")
             for i in [self.index]:
                 for t in self.days:
@@ -340,6 +339,7 @@ while (modelImprovable) and itr < max_itr:
             master.addLambda(index, itr)
             master.updateModel()
             modelImprovable = True
+            print(f"Reduced-cost < 0 columns found...")
     master.updateModel()
     master.model.write(f"LP-Iteration-{itr}.lp")
 

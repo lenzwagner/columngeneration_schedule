971637a43a0733c50b6f86ce7984f65d6f77f384
lenzwagner <lorenz.wagner99@gmail.com>
2024-03-07 17:29:19 +0100

    Outsource plots

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ import gurobipy as gu
 import pandas as pd
 import os
 import time
-import seaborn as sns
-import matplotlib.pyplot as plt
+from plots import plot_obj_val, plot_avg_rc, plot_together
 import random
 
 clear = lambda: os.system('cls')
@@ -412,49 +411,6 @@ elif round((time.time() - t0), 4) > time_problem:
 else:
     print("*{:^78}*".format("CG and normal solver are equally fast: " + str(time_problem) + " seconds"))
 print("*" * 80)
-
-def plot_obj_val(objValHistRMP):
-    sns.set(style='darkgrid')
-    sns.scatterplot(x=list(range(len(objValHistRMP))), y=objValHistRMP, marker='o')
-    sns.lineplot(x=list(range(len(objValHistRMP))), y=objValHistRMP)
-    plt.xlabel('CG Iterations')
-    plt.xticks(range(0, len(objValHistRMP)))
-    plt.ylabel('Objective function value')
-    title = 'Optimal objective value: ' + str(round(objValHistRMP[-1], 2))
-    plt.title(title)
-    plt.show()
-
-def plot_avg_rc(avg_rc_hist):
-    sns.set(style='darkgrid')
-    sns.scatterplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist, marker='o')
-    sns.lineplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist)
-    plt.xlabel('CG Iterations')
-    plt.xticks(range(1, len(avg_rc_hist)+1))
-    plt.ylabel('Reduced Cost')
-    title = 'Final reduced cost: ' + str(round(avg_rc_hist[-1], 2))
-    plt.title(title)
-    plt.show()
-
-def plot_together(objValHistRMP, avg_rc_hist):
-    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-
-    sns.scatterplot(x=list(range(len(objValHistRMP))), y=objValHistRMP, marker='o', ax=axs[0])
-    sns.lineplot(x=list(range(len(objValHistRMP))), y=objValHistRMP, ax=axs[0])
-    axs[0].set_xlabel('CG Iterations')
-    axs[0].set_xticks(range(0, len(objValHistRMP)))
-    axs[0].set_ylabel('Objective function value')
-    title = 'Optimal objective value: ' + str(round(objValHistRMP[-1], 2))
-    axs[0].set_title(title)
-
-    sns.scatterplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist, marker='o', ax=axs[1])
-    sns.lineplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist, ax=axs[1])
-    axs[1].set_xlabel('CG Iterations')
-    axs[1].set_xticks(range(1, len(avg_rc_hist)+1))
-    axs[1].set_ylabel('Reduced Cost')
-    title = 'Final reduced cost: ' + str(round(avg_rc_hist[-1], 2))
-    axs[1].set_title(title)
-
-    plt.show()
 
 # Plots
 plot_obj_val(objValHistRMP)
---
+++ plots.py
@@ -0,0 +1,44 @@
+import seaborn as sns
+import matplotlib.pyplot as plt
+def plot_obj_val(objValHistRMP):
+    sns.set(style='darkgrid')
+    sns.scatterplot(x=list(range(len(objValHistRMP))), y=objValHistRMP, marker='o')
+    sns.lineplot(x=list(range(len(objValHistRMP))), y=objValHistRMP)
+    plt.xlabel('CG Iterations')
+    plt.xticks(range(0, len(objValHistRMP)))
+    plt.ylabel('Objective function value')
+    title = 'Optimal objective value: ' + str(round(objValHistRMP[-1], 2))
+    plt.title(title)
+    plt.show()
+
+def plot_avg_rc(avg_rc_hist):
+    sns.set(style='darkgrid')
+    sns.scatterplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist, marker='o')
+    sns.lineplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist)
+    plt.xlabel('CG Iterations')
+    plt.xticks(range(1, len(avg_rc_hist)+1))
+    plt.ylabel('Reduced Cost')
+    title = 'Final reduced cost: ' + str(round(avg_rc_hist[-1], 2))
+    plt.title(title)
+    plt.show()
+
+def plot_together(objValHistRMP, avg_rc_hist):
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+
+    sns.scatterplot(x=list(range(len(objValHistRMP))), y=objValHistRMP, marker='o', ax=axs[0])
+    sns.lineplot(x=list(range(len(objValHistRMP))), y=objValHistRMP, ax=axs[0])
+    axs[0].set_xlabel('CG Iterations')
+    axs[0].set_xticks(range(0, len(objValHistRMP)))
+    axs[0].set_ylabel('Objective function value')
+    title = 'Optimal objective value: ' + str(round(objValHistRMP[-1], 2))
+    axs[0].set_title(title)
+
+    sns.scatterplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist, marker='o', ax=axs[1])
+    sns.lineplot(x=list(range(1, len(avg_rc_hist) + 1)), y=avg_rc_hist, ax=axs[1])
+    axs[1].set_xlabel('CG Iterations')
+    axs[1].set_xticks(range(1, len(avg_rc_hist)+1))
+    axs[1].set_ylabel('Reduced Cost')
+    title = 'Final reduced cost: ' + str(round(avg_rc_hist[-1], 2))
+    axs[1].set_title(title)
+
+    plt.show()
